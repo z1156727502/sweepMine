ef4bd0af81fc0387a90daf64ce6167ccc7e5233d
dwqc <1156727502@qq.com>
2018-04-27 00:02:10 +0800

    待动态创建雷区

---
+++ src/aweepMine.js
@@ -0,0 +1,9 @@
+var sweepMine = {
+    dif : 1,
+    num : 10,
+    init : function (){},
+    setMine : function(){},
+    creatDom : function(){
+        $('.wrapper').append('<li></li>');
+    }
+}
